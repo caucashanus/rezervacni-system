ce7c70e5aa39342df8272f7a1906223fe956c829
caucashanus <caucashanus@gmail.com>
2025-07-17 17:14:11 +0200

    Delete realbarber-master directory

--- realbarber-master/src/app/calendar/page.tsx
+++
@@ -1,5 +0,0 @@
-import Calendar from '../../components/Calendar';
-
-export default function CalendarPage({ searchParams }: { searchParams?: { date?: string, locationId?: string, key?: string } }) {
-  return <Calendar searchParams={searchParams} />;
-} 
